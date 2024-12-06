b3dc98e05a821329a709aebc59ed4b7e0edc1abb
Mega <momagdy010299@gmail.com>
2024-12-06 19:40:03 +0200

    DB connection

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(con => {
+    console.log('DB connection successful!');
+}).catch(err => {
+    console.log('Database connection error:  '+err);
+})
 const app = require('./index.js');
 
 const port = process.env.PORT || 3000;
